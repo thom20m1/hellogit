38a148e59603ff18ee0045e2542a4c13763b778c
thom20m1 <45196128+thom20m1@users.noreply.github.com>
2018-11-20 09:16:03 +0000

    Update Program.cs
    Added Comment

--- hellogit/Program.cs
+++ hellogit/Program.cs
@@ -11,7 +11,7 @@ namespace hellogit
         static void Main(string[] args)
         {
             Console.WriteLine("Hello GIT");
-            Console.ReadLine();
+            Console.ReadLine(); //Borkborkbork
         }
     }
 }
